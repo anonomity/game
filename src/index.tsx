8e8578f6a7315bad32c052d735243f257f2db8bc
Jackelyn Velez <jlauravelez@gmail.com>
2020-12-12 22:04:01 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles, theme } from './styles';
+import { Content, Title, Card } from './components';
 import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        This is a work in progress
+      </Card>
+    </Content>
+  </ ThemeProvider>,
   document.getElementById('root')
 );
 
